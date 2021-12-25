f5af4e67017c3208ff7e38fe811b0c46e6ca834e
shimdevkun <shimdevkun@gmail.com>
2021-12-25 16:25:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Snipper Landing Page
+
+Personal solution for the "Snipper Landing Page" challenge by Codewell
+
+[Live version](#)
+
+## Technologies Used
+
+## Results
+
+### Desktop Version
+
+### Mobile Version
+
+## See also
+
+[Link to the challenge](https://www.codewell.cc/challenges/snipper-landing-page--608bbe67e0984a001540d79b)
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyles() {
+  return src("css/index.scss").pipe(sass()).pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["css/index.scss"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
